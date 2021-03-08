1008f3c3fd47419651610ad9022ea6c378c72c6a
HeyFang <heyfang36@gmail.com>
2021-03-08 19:40:03 +0530

    08-03-2021/19:40

--- cogs/filter.py
+++ cogs/filter.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands, tasks
 
-bot = commands.Bot(command_prefix=":", intents = discord.Intents.all())
 
 
 
--- index.py
+++ index.py
@@ -127,6 +127,66 @@ async def serverinfo(ctx):
 
 
 
+#Repeatmessage cmd imported from alice...
+@bot.command()
+@commands.has_permissions(administrator= True)
+async def repeatmessage(ctx, enabled="start", interval=15, message=""):
+    if enabled.lower() == "stop":
+        messageInterval.stop()
+    elif enabled.lower() == "start":
+        messageInterval.change_interval(seconds=int(interval))
+        messageInterval.start(ctx,message)
+@tasks.loop(seconds=15)
+async def messageInterval(ctx,message):
+    await ctx.send(message)
+
+
+
+#cmd mute
+@bot.command(description="Mutes the specified user.")
+@commands.has_permissions(manage_messages=True)
+async def mute(ctx, member: discord.Member, *, reason=None):
+    guild = ctx.guild
+    mutedRole = discord.utils.get(guild.roles, name="Muted")
+
+    if not mutedRole:
+        mutedRole = await guild.create_role(name="Muted")
+
+        for channel in guild.channels:
+            await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
+    
+    em = discord.Embed(title = None,
+     description = f" *:white_check_mark: **{member.mention}** was muted for reason {reason}*",
+      color = discord.Color.green())
+
+    await member.add_roles(mutedRole, reason=reason)
+    await member.send(f"You were muted in the server {guild.name} for {reason}")
+    await ctx.send(embed=em)
+
+
+#unmute
+@bot.command(description="unmutes user")
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+    em = discord.Embed(title = None,
+     description = f" *:white_check_mark: **{member.mention}** was successfully unmuted!*",
+      color = discord.Color.green())
+
+    await member.remove_roles(mutedRole)
+    await member.send(f"You were unmuted in the server {ctx.guild.name}")
+    await ctx.send(embed=em)
+
+
+
+
+
+
+
+
+
+
 
 
 
